26504a789e51afa7f247a966d79d441f24977732
derlongloi <derlong.loi@salesworksgroup.com>
2026-04-28 10:57:17 +0800

    tidy up and login plus register

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from datetime import date
+
+import werkzeug
 from flask import Flask, abort, render_template, redirect, url_for, flash, request
 from flask_bootstrap import Bootstrap5
 from flask_ckeditor import CKEditor
 from flask_gravatar import Gravatar
-from flask_login import UserMixin, login_user, LoginManager, current_user, logout_user
+from flask_login import UserMixin, login_user, LoginManager, current_user, logout_user, login_required
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.exc import IntegrityError
 from sqlalchemy.orm import relationship, DeclarativeBase, Mapped, mapped_column
@@ -11,7 +13,7 @@ from sqlalchemy import Integer, String, Text
 from functools import wraps
 from werkzeug.security import generate_password_hash, check_password_hash
 # Import your forms from the forms.py
-from forms import CreateClientForm, CreatePartForm
+from forms import CreateClientForm, CreatePartForm, RegisterForm, LoginForm
 
 from db_config import db, init_db
 from db_model import QUser, QClient, QPart
@@ -60,29 +62,81 @@ with app.app_context():
 
 
 # TODO: Use Werkzeug to hash the user's password when creating a new user.
-@app.route('/register')
+@app.route('/register', methods=['GET', 'POST'])
 def register():
-    return render_template("register.html")
+    new_user_form = RegisterForm()
+    if new_user_form.validate_on_submit():
+        # hash and salt user entered password
+        hash_salt_password = werkzeug.security.generate_password_hash(
+            new_user_form.password.data,
+            method='pbkdf2:sha256',
+            salt_length=8)
+        entered_email = new_user_form.email.data.strip().lower()
+        register_user = QUser(
+            name=new_user_form.name.data,
+            email=entered_email,
+            password=hash_salt_password)
+        # check if new register user email already exist
+        result = db.session.execute(db.select(QUser).where(QUser.email == entered_email))
+        found_db_user = result.scalar()
+        # print(f"result scalar={db_user}")
+        if found_db_user:
+            flash(f"Email address {entered_email} already registered!", 'error')
+        else:
+            try:
+                # add new user into db table
+                db.session.add(register_user)
+                db.session.commit()
+            except IntegrityError as e:
+                # catch error
+                db.session.rollback()
+                # flash message for error
+                flash(f'error: {e}', 'error')
+            else:
+                # if add user return no error
+                login_user(register_user)
+                # flash('Logged in successfully.', 'info')
+                return redirect(url_for("home"))
+    return render_template("register.html", form=new_user_form)
 
 
 # TODO: Retrieve a user from the database based on their email. 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template("login.html")
+    login_form = LoginForm()
+    if login_form.validate_on_submit():
+        entered_email = login_form.email.data
+        entered_password = login_form.password.data
+        # check db for entered email
+        result = db.session.execute(db.select(QUser).where(QUser.email == entered_email))
+        found_db_user = result.scalar()
+        if not found_db_user:
+            flash(message="User email address not found! Please register.", category='error')
+        else:
+            password_matched = werkzeug.security.check_password_hash(found_db_user.password, entered_password)
+            if password_matched:
+                # flash(message="Verified entered password. Login successfully.", category='info')
+                login_user(found_db_user)
+                return redirect(url_for('home'))
+            else:
+                flash(message="Wrong password. Please try again.", category='error')
+    return render_template("login.html", form=login_form, current_user=current_user)
 
 
 @app.route('/logout')
+@login_required
 def logout():
-    return redirect(url_for('get_all_posts'))
+    logout_user()
+    return redirect(url_for('home'))
 
 
 @app.route('/')
-def get_all_posts():
-    result = db.session.execute(db.select(BlogPost))
-    posts = result.scalars().all()
-    return render_template("index.html", all_posts=posts)
+@login_required
+def home():
+    return redirect(url_for('get_all_client'))
 
 @app.route('/client')
+@login_required
 def get_all_client():
     result = db.session.execute((db.select(QClient)))
     clients = result.scalars().all()
@@ -90,12 +144,14 @@ def get_all_client():
     return render_template(
         "list-client.html",
         table_header=client_t_header,
-        all_client=clients
+        all_client=clients,
+        current_user=current_user
     )
 
 
-# TODO: Allow logged-in users to comment on posts
+# TODO: Route to list part for selected client.
 @app.route("/client-part/<int:client_id>")
+@login_required
 def get_client_part(client_id):
     requested_client = db.get_or_404(QClient, client_id)
     part_t_header=["Manufacturer", "Model", "Serial number", "Shipping date", "Inspected", "Remark",
@@ -106,12 +162,14 @@ def get_client_part(client_id):
         "list-part.html",
         table_header=part_t_header,
         all_part=parts,
-        client=requested_client
+        client=requested_client,
+        current_user=current_user
     )
 
 
-# TODO: Use a decorator so only an admin user can create a new post
+# TODO: Route to add new client.
 @app.route("/new-client", methods=["GET", "POST"])
+@login_required
 def add_new_client():
     form = CreateClientForm()
     if form.validate_on_submit():
@@ -132,9 +190,15 @@ def add_new_client():
             flash(f'error: {msg}', 'error')
         else:
             return redirect(url_for("get_all_client"))
-    return render_template("make-client.html", form=form)
+    return render_template(
+        "make-client.html",
+        form=form,
+        current_user=current_user
+    )
 
+# TODO: Route to add new part for selected client. clientId passed in as URL argument.
 @app.route("/new-part", methods=["GET", "POST"])
+@login_required
 def add_new_part():
     client_id = request.args.get("clientId", type=str)
     requested_client = db.get_or_404(QClient, client_id)
@@ -159,7 +223,9 @@ def add_new_part():
         "make-part.html",
         form=create_part_form,
         action="create",
-        client=requested_client)
+        client=requested_client,
+        current_user=current_user
+    )
 
 
 # TODO: Use a decorator so only an admin user can edit a post
